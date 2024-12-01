7e0d3b97d620e44d097a3caa3c7bd7a1ea987aea
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-12-01 22:17:38 +0000

    Add Category model and CategoryProvider for managing news categories in the app

---
+++ app/src/main/java/app/yournewsontime/data/model/Category.kt
@@ -0,0 +1,6 @@
+package app.yournewsontime.data.model
+
+data class Category(
+    val name: String,
+    var isFollowed: Boolean = false
+)
---
+++ app/src/main/java/app/yournewsontime/data/model/CategoryProvider.kt
@@ -0,0 +1,29 @@
+package app.yournewsontime.data.model
+
+object CategoryProvider {
+    val categories = listOf(
+        Category("Arts"),
+        Category("Automobiles"),
+        Category("Blogs"),
+        Category("Books"),
+        Category("Business"),
+        Category("Crosswords & Games"),
+        Category("Dining & Wine"),
+        Category("Education"),
+        Category("Fashion & Style"),
+        Category("Food"),
+        Category("Health"),
+        Category("Home & Garden"),
+        Category("Learning"),
+        Category("Magazine"),
+        Category("Movies"),
+        Category("Multimedia"),
+        Category("Olympics"),
+        Category("Science"),
+        Category("Sports"),
+        Category("Style"),
+        Category("Technology"),
+        Category("Theater"),
+        Category("Travel")
+    )
+}
