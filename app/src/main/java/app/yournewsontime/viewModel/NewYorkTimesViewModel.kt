31a3470b22b40e31376e788b03b978e0fc288f54
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-12-01 22:58:54 +0000

    Delete println on NewYorkTimesViewModel.kt

--- app/src/main/java/app/yournewsontime/viewModel/NewYorkTimesViewModel.kt
+++ app/src/main/java/app/yournewsontime/viewModel/NewYorkTimesViewModel.kt
@@ -12,11 +12,15 @@ class NewYorkTimesViewModel(private val repository: NewYorkTimesRepository) : Vi
     val articles = mutableStateOf<List<Article>>(emptyList())
     val errorMessage = mutableStateOf<String?>(null)
 
-    fun fetchArticles(category: String, apiKey: String, beginDate: String? = null, endDate: String? = null) {
+    fun fetchArticles(
+        category: String,
+        apiKey: String,
+        beginDate: String? = null,
+        endDate: String? = null
+    ) {
         viewModelScope.launch(Dispatchers.IO) {
             if (beginDate != null && endDate != null) {
                 repository.searchArticles(category, apiKey, beginDate, endDate) { result, error ->
-                    println("Aqui estoy: $category $apiKey $beginDate $endDate")
                     if (error != null) {
                         errorMessage.value = error
                     } else {
